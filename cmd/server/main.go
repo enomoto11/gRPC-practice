e695553aa3e3cee98a5e4bc5194d66e97357df5a
Togo Enomoto <t.enomoto@catallaxy.me>
2024-04-12 02:49:41 +0900

    ✨ mainを追加

---
+++ cmd/server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	hellopb "mygrpc/pkg/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := 8081
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	server := grpc.NewServer()
+
+	hellopb.RegisterGreetingServiceServer(server, NewMyServer())
+
+	// 4. サーバーリフレクションの設定
+	reflection.Register(server)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		server.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	server.GracefulStop()
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
